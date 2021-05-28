67ec4fea3f77eacc72cd6a79b34a51b3e4ef4a1d
Javier <javier.gauna147@gmail.com>
2021-05-27 21:42:19 -0300

    estructura de carpetas finalizada

--- Proyecto/front-end/src/app/app-routing.module.ts
+++ Proyecto/front-end/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home-component/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: '/home' }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
