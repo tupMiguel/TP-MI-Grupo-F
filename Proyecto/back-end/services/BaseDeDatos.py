70fba69602126564e1abe3d2eb5e3e15f3ed1458
AgusAngeloni <82539007+AgusaAngeloni@users.noreply.github.com>
2021-05-29 21:23:08 -0300

    agrego clase BaseDeDatos

---
+++ Proyecto/back-end/services/BaseDeDatos.py
@@ -0,0 +1,26 @@
+import os
+
+class BaseDeDatos():
+    
+    __instancia=None
+    
+    #Singleton
+    def __new__(cls):
+        if cls.__instancia is None:
+           cls.__instancia= object.__new__(cls)
+        return cls.__instancia
+    
+    #Agrego String al archivo
+    def guardarString(self, linea):
+        archivo=open("/back-end/storage/storage.txt ","a") 
+        archivo.write("\n"+linea)
+        archivo.close()
+
+    #Lectura de String del archivo
+    def leerString(self, num=0):
+        archivo=open("/back-end/storage/storage.txt ","r") 
+        lineas=archivo.readlines()
+        frase=lineas[num]
+        print(frase)
+        return frase
+    
