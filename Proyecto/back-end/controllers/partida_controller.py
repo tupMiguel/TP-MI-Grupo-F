6fef3e1b463e621302f2e783901f17be4fe9168e
Miguel <miguel.condori@alumnos.frm.utn.edu.ar>
2021-05-30 22:23:34 -0300

    agregando funcionalidad para mezclar paises y asignar paises en la matriz tablero, con los respectivos imports necesarios

--- Proyecto/back-end/controllers/partida_controller.py
+++ Proyecto/back-end/controllers/partida_controller.py
@@ -1,7 +1,9 @@
 from models.partida_model import PartidaModel
+from models import pais_model 
 from dataclasses import dataclass
 from services.baseDeDatos_service import BaseDeDatosService
-
+from globals import PAISES
+import random
 
 @dataclass
 class PartidaController:
@@ -15,3 +17,23 @@ class PartidaController:
             partida.tablero[coordX][coordY].duenioId = idJugadorAtancante
 
         self.baseDeDatos.add_data(str(partida.__dict__));
+
+    def colocarPaises(self, matriz):
+        """
+        matriz: array bidimensional, llena de 0 y 1
+        return: array bidimensional, reemplazando cada 1 por un objeto PaisModel
+        """
+        contador = 0
+        for fila in matriz:
+            for columna in matriz:
+                if matriz[fila][columna] == 1:
+                    matriz[fila][columna] = pais_model.PaisModel(PAISES[contador],"","")
+                    contador += 1 
+        return matriz
+    
+    def mezclarPaises(self, lista_Paises):
+        """
+        lista_Paises: lista[str], array de strings conteniendo nombres de paises
+        return: None
+        """
+        random.shuffle(lista_Paises)
