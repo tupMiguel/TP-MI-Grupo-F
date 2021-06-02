ba21c0f9ee30ff17c38f216bdd17d086a600c499
JuanCortinez <juan.cortinez@alumnos.frm.utn.edu.ar>
2021-06-02 13:36:29 -0300

    se agrega funciones de obtener lista de paises aleatorios para cada jugador

--- Proyecto/back-end/back-end_j1/asignacionPaises_model.py
+++ Proyecto/back-end/back-end_j1/asignacionPaises_model.py
@@ -1,6 +1,7 @@
 #funcion que define la asignacion de los paises a cada jugador
 import random
 from paisesDisponibles_model import *
+from random import shuffle
 #from jugador_model import Jugador
 #from pais_model import Pais
 
@@ -34,22 +35,50 @@ class asignacionDePaises():
     matrizConPaises = [[]]
     #llamada al metodo solo para ver el funcionamiento
     matrizConPaises = crearMatrizPais(matrizDePrueba)
+    print("Matriz con paises y Agua: ")
+    print(" ")
     print (matrizConPaises)
 
     
     #separo los paises de la matriz
     def ObtenerListaPaises(matriz):
-        listaPaisesOrdenada = []
-        
+        listaPaises = []
         for fila in range(len(matriz)):
             for columna in range(len(matriz[0])):
                 if matriz[fila][columna] != 'Agua':
-                    listaPaisesOrdenada.append(matriz[fila][columna])
+                    listaPaises.append(matriz[fila][columna])
                     
         
-        return(listaPaisesOrdenada)    
+        return(listaPaises)    
     
     #llamada al metodo solo para ver el funcionamiento
-    listaDesordenada = asignarPaisesAJugadores(matrizConPaises)
+    listaPaisesAleatorios = ObtenerListaPaises(matrizConPaises)
     #los paises se tienen que asignar de acuerdo a la cantidad de jugadores
-    print(listaDesordenada)
+    print(" ")
+    print(" Lista de solo paises ")
+    print(" ")
+    print(listaPaisesAleatorios)
+    #desordeno la lista para que la eleccion sea al azar
+    shuffle(listaPaisesAleatorios)
+    print(" ")
+    print(" Lista de paises mezclados para asignarlos al azar ")
+    print(" ")
+    print(listaPaisesAleatorios)
+
+    #funciones para dividir la lista de acuerdo a la cantidad de jugadores
+
+    def dividirListaEn2(lista_paises):
+        mitad = len(lista_paises)//2
+        return (lista_paises[:mitad], lista_paises[mitad:])
+    
+    def asignarPaises(cantJugadores):
+        if cantJugadores == 2:
+            listaPaisesJugador1, listaPaisesJugador2 = dividirListaEn2(listaPaisesAleatorios)
+        return (listaPaisesJugador1, listaPaisesJugador2)           
+         
+    listaPaisesJugador1, listaPaisesJugador2 = asignarPaises(2)
+
+    print("lista paises jugador 1:")
+    print(listaPaisesJugador1)
+    print("lista paises jugador 2:")
+    print(listaPaisesJugador2)
