79aa34cc7592b2c7f0f00b7410ce9c3ecbc1f11e
JuanCortinez <juan.cortinez@alumnos.frm.utn.edu.ar>
2021-06-06 20:35:51 -0300

    se agregan funciones para definir los paises a 2,3 y 4 jugadores

--- Proyecto/back-end/back-end_j1/asignacionPaises_model.py
+++ Proyecto/back-end/back-end_j1/asignacionPaises_model.py
@@ -1,47 +1,52 @@
-#funcion que define la asignacion de los paises a cada jugador
+#funciones que definen la asignacion de los paises a cada jugador
+#los print solo se colocan solo a modo de ejemplo para ver la salida de las funciones
+
 import random
 from paisesDisponibles_model import *
 from random import shuffle
-#from jugador_model import Jugador
-#from pais_model import Pais
 
 
-class asignacionDePaises():
+
+#class asignacionDePaises():
     # funcion que recibe una matriz de 0 y 1, los '1' los reemplaza por paises elegidos al azar, tambien guarda la posicion.
     # nunca se repiten los paises aunque la matriz sea la misma
-    def crearMatrizPais(matriz):
-        for fila in range(len(matriz)):
-            for columna in range(len(matriz[0])):
-                if matriz[fila][columna] == 1:
-                    nuevoPais = [random.choice(PAISES),fila,columna]
-                    matriz[fila][columna] = nuevoPais
-                    
-                else:
-                    nuevoPais = "Agua"
-                    matriz[fila][columna] = nuevoPais
+def crearMatrizPais(matriz):
+    paisesParaElegir = random.sample(PAISES, 50)
+    print("PAISES PARA ELEGIR:")
+    print(paisesParaElegir)
+    cont = 0
+    for fila in range(len(matriz)):
+        for columna in range(len(matriz[0])):
+            if matriz[fila][columna] == 1:
+                nuevoPais = [paisesParaElegir[cont],fila,columna]
+                matriz[fila][columna] = nuevoPais
+                cont = cont + 1    
+            else:
+                nuevoPais = "Agua"
+                matriz[fila][columna] = nuevoPais
 
-        return matriz   
+    return matriz   
 
 
 
     #matriz a modo de ejemplo
-    matrizDePrueba = [
+matrizDePrueba = [
     [0,1,1,1,0,1],
     [1,0,1,1,1,1],
-    [1,1,1,1,0,0]
+    [1,1,0,1,0,0]
      ]  
     
     
-    matrizConPaises = [[]]
+matrizConPaises = [[]]
     #llamada al metodo solo para ver el funcionamiento
-    matrizConPaises = crearMatrizPais(matrizDePrueba)
-    print("Matriz con paises y Agua: ")
-    print(" ")
-    print (matrizConPaises)
+matrizConPaises = crearMatrizPais(matrizDePrueba)
+print("MATRIZ CON PAISES Y AGUA ")
+print(" ")
+print (matrizConPaises)
 
     
     #separo los paises de la matriz
-    def ObtenerListaPaises(matriz):
+def ObtenerListaPaises(matriz):
         listaPaises = []
         for fila in range(len(matriz)):
             for columna in range(len(matriz[0])):
@@ -52,33 +57,79 @@ class asignacionDePaises():
         return(listaPaises)    
     
     #llamada al metodo solo para ver el funcionamiento
-    listaPaisesAleatorios = ObtenerListaPaises(matrizConPaises)
+listaPaisesAleatorios = ObtenerListaPaises(matrizConPaises)
     #los paises se tienen que asignar de acuerdo a la cantidad de jugadores
-    print(" ")
-    print(" Lista de solo paises ")
-    print(" ")
-    print(listaPaisesAleatorios)
+print(" ")
+print(" LISTA DE SOLO PAISES: ")
+print(" ")
+print(listaPaisesAleatorios)
     #desordeno la lista para que la eleccion sea al azar
-    shuffle(listaPaisesAleatorios)
-    print(" ")
-    print(" Lista de paises mezclados para asignarlos al azar ")
-    print(" ")
-    print(listaPaisesAleatorios)
+shuffle(listaPaisesAleatorios)
+print(" ")
+print(" LISTA DE PAISES MEZCLADOS PARA ASIGNARLOS AL AZAR: ")
+print(" ")
+print(listaPaisesAleatorios)
 
-    #funciones para dividir la lista de acuerdo a la cantidad de jugadores
-
-    def dividirListaEn2(lista_paises):
-        mitad = len(lista_paises)//2
-        return (lista_paises[:mitad], lista_paises[mitad:])
     
-    def asignarPaises(cantJugadores):
-        if cantJugadores == 2:
-            listaPaisesJugador1, listaPaisesJugador2 = dividirListaEn2(listaPaisesAleatorios)
-        return (listaPaisesJugador1, listaPaisesJugador2)           
-         
-    listaPaisesJugador1, listaPaisesJugador2 = asignarPaises(2)
+#funcion que divide la lista de paises de acuerdo a la cantidad de jugadores
+def dividirLista(lista_paises, cantJug):
+        if cantJug == 2:
+            mitad = len(lista_paises)//2
+            return (lista_paises[:mitad], lista_paises[mitad:])
+        if cantJug == 3:
+            div = len(lista_paises)//3
+            return (lista_paises[:div], lista_paises[div:div*2], lista_paises[div*2:])
+        if cantJug == 4:
+            div = len(lista_paises)//4
+            return (lista_paises[:div], lista_paises[div:div*2], lista_paises[div*2:div*3], lista_paises[div*3:])    
 
-    print("lista paises jugador 1:")
-    print(listaPaisesJugador1)
-    print("lista paises jugador 2:")
-    print(listaPaisesJugador2)
+
+                
+
+#funcion donde se ingresan la cantidad de jugadores para luego asignarle paises
+def jugadorConPaises(cantPlayer):
+    
+    if cantPlayer == 2:
+        listaPaisesJugador1, listaPaisesJugador2 = dividirLista(listaPaisesAleatorios,2)
+        print(" ")
+        print("lista paises jugador 1:")
+        print(listaPaisesJugador1)
+        print(" ")
+        print("lista paises jugador 2:")
+        print(listaPaisesJugador2) 
+    if cantPlayer == 3:
+        listaPaisesJugador1, listaPaisesJugador2, listaPaisesJugador3= dividirLista(listaPaisesAleatorios,3)
+        print(" ")
+        print("lista paises jugador 1:")
+        print(listaPaisesJugador1)
+        print(" ")
+        print("lista paises jugador 2:")
+        print(listaPaisesJugador2)
+        print(" ")
+        print("lista paises jugador 3:")
+        print(listaPaisesJugador3)
+    if cantPlayer == 4:
+        listaPaisesJugador1, listaPaisesJugador2, listaPaisesJugador3, listaPaisesJugador4 = dividirLista(listaPaisesAleatorios,4)
+        print(" ")
+        print("lista paises jugador 1:")
+        print(listaPaisesJugador1)
+        print(" ")
+        print("lista paises jugador 2:")
+        print(listaPaisesJugador2)
+        print(" ")
+        print("lista paises jugador 3:")
+        print(listaPaisesJugador3)    
+        print(" ")
+        print("lista paises jugador 4:")
+        print(listaPaisesJugador4) 
+
+#llamada a la funcion solo para revisar su funcionamiento
+print("")
+print("EJEMPLO PARA DOS JUGADORES: ")
+jugadorConPaises(2)
+print("")
+print(("EJEMPLO PARA TRES JUGADORES:"))
+jugadorConPaises(3)
+print("")
+print(("EJEMPLO PARA CUATRO JUGADORES:"))
+jugadorConPaises(4)
